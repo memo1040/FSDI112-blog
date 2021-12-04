fb937e89623837c3b0fc1f4d99e50c778c2263d1
Guillermo Monge <gmt1040@gmail.com>
2021-12-04 11:46:48 -0800

    Added accounts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import (
     TemplateView,
     ListView,
@@ -21,24 +25,36 @@ class BlogListView(ListView):
     template_name = "blog_list.html"
 
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "blog_detail.html"
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "blog_new.html"
-    fields = ['title', 'body', 'author']
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "blog_edit.html"
     fields = ['title', 'body']
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class BlogDeleteView(DeleteView):
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog_delete.html"
     success_url = reverse_lazy("blog_list")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
